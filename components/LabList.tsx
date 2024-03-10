4d1d5034161bd946c564b0beb13730d7a1f2cb69
Markusplay <mshilo2004@gmail.com>
2024-03-10 23:53:22 +0200

    updated report template

--- components/LabList.tsx
+++ components/LabList.tsx
@@ -2,10 +2,15 @@ const LabList = () => {
     const labs=["/lab1","/lab2","/lab3","/lab4","/lab5"]
     return (
         <>
-            <h2>Звіти з лабораторних робіт</h2>
-            <h2>З дисципліни "Web-Орієнтовані технології.</h2>
-            <h2>Основи Frontend та Backend розробок"</h2>
-            <h3>Студент групи ІС-11 Шило Михайло Олександрович</h3>
+            <div style={{display:'flex', justifyContent:'space-around'}}>
+                <div>
+                    <h2>Звіти з лабораторних робіт</h2>
+                    <h2>З дисципліни "Web-Орієнтовані технології.</h2>
+                    <h2>Основи Frontend та Backend розробок"</h2>
+                    <h3>Студент групи ІС-11 Шило Михайло Олександрович</h3>
+                </div>
+                <img style={{width:'200px'}} src="me.jpg"/>
+            </div>
             <div style={{display: 'flex', gap: '10px'}}>
 
                 {
