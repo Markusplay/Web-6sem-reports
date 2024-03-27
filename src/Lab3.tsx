dcd69731b2f1ea60ced737bb337d4a673143e738
Markusplay <mshilo2004@gmail.com>
2024-03-27 14:34:39 +0200

    fixed task 3 router

--- src/Lab3.tsx
+++ src/Lab3.tsx
@@ -13,14 +13,14 @@ const Lab3 = () => {
                     Мета
                     Місце розташування лаби №3
                 </button>
-                <button style={{width: 'fit-content'}}><a href="Labs3/Task1/index.html">Завдання №1. Зовнішній вигляд макету. Код макету</a></button>
+                <button style={{width: 'fit-content'}} onClick={() => setTab(11)}><a href="Labs3/Task1/index.html">Завдання №1. Зовнішній вигляд макету. Код макету</a></button>
                 <button style={{width: 'fit-content'}}>Завдання №2</button>
                 <a href="Labs3/Task2-1/index.html">Фіксована таблична верстка</a>
                 <a href="Labs3/Task2-2/index.html">Гумова таблична верстка</a>
                 <a href="Labs3/Task2-3/index.html">Фіксована блокова верстка</a>
                 <a href="Labs3/Task2-4/index.html">Гумова блокова верстка</a>
                 <button style={{width: 'fit-content'}}>Завдання №3 FLEXBOX</button>
-                <button style={{width: 'fit-content'}} onClick={() => setTab(2)}>Зовнішній вигляд</button>
+                <a href="Labs3/Task3-1/index.html">Зовнішній вигляд</a>
                 <button style={{width: 'fit-content'}} onClick={() => setTab(3)}>HTML-код</button>
                 <button style={{width: 'fit-content'}} onClick={() => setTab(4)}>CSS-код сторінки</button>
                 <button style={{width: 'fit-content'}} onClick={() => setTab(5)}>Висновки</button>
@@ -44,16 +44,22 @@ const Lab3 = () => {
                         Проєкт: <a>https://github.com/Markusplay/pizzeria</a>
                     </p>
                     }
+                    {tab === 11 &&
+                    <div style={{width:'100%'}}>
+                        <img src="lab3/maket.png" style={{width:'800px', height:'1200px'}}/>
+                        <img src="lab3/maket-code.png" style={{width:'800px', height:'1200px'}}/>
+                    </div>
+                    }
                     {tab === 2 &&
                     <div style={{width:'100%'}}>
                         <h3>Зовнішній стилі</h3>
-                        <img src="lab3/res.png" style={{ objectFit:'none'}}/>
+                        <img src="lab3/res.png" style={{width:'800px', height:'1200px'}}/>
                     </div>
                     }
                     {tab === 3 &&
                     <div style={{width:'100%'}}>
                         <h3>HTML код</h3>
-                        <img src="lab3/html-code.png" style={{width:'800px',height:'1200px'}}/>
+                        <img src="lab3/html-code.png" style={{width:'800px', height:'1200px'}}/>
                     </div>
                     }
                     {tab === 4 &&
